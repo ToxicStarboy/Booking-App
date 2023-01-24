9ee02f918127363f9c6a2ce17c110a7bf770bca8
Aadesh Gupta <aadeshgupta2003@gmail.com>
2023-01-24 11:20:33 +0530

    build folder in api

---
+++ api/build/precache-manifest.3a2e0d6d230ea5d7cdaf03aa3bd3bf30.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f914169172711a3d2e1eb13903906962",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0a33d0cc3746dd93a64",
+    "url": "/static/css/2.aa8be15f.chunk.css"
+  },
+  {
+    "revision": "d498a5b3983dfc50ecfe",
+    "url": "/static/css/main.a222c08a.chunk.css"
+  },
+  {
+    "revision": "d0a33d0cc3746dd93a64",
+    "url": "/static/js/2.8cb0fbde.chunk.js"
+  },
+  {
+    "revision": "3b6cebb30f41345d7132ece218270c72",
+    "url": "/static/js/2.8cb0fbde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d498a5b3983dfc50ecfe",
+    "url": "/static/js/main.81fc3a91.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.81fc3a91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e2151b394ef91cb5932",
+    "url": "/static/js/runtime-main.c85524a0.js"
+  }
+]);
