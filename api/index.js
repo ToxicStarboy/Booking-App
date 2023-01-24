3754fce9521023d36ff2d238eb0a1945420327d3
Aadesh Gupta <aadeshgupta2003@gmail.com>
2023-01-24 11:36:43 +0530

    serving html file in api

--- api/index.js
+++ api/index.js
@@ -11,7 +11,7 @@ import { fileURLToPath } from "url";
 import { dirname } from "path";
 
 const __filename = fileURLToPath(import.meta.url);
-const _dirname = dirname(_filename);
+const __dirname = dirname(__filename);
 
 const app = express();
 dotenv.config();
